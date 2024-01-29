ac7f2b26d7326a3981b9b509a7157a77f2feb3b1
Gourav Sharma <gourav.sharma@edu.sait.ca>
2024-01-29 14:22:41 -0700

    first

---
+++ cprg306-assignments/app/week-2/student-info.js
@@ -0,0 +1,8 @@
+import Link from "next/link";
+
+export default function StudentInfo () {
+    return(
+        <><p>Gourav Sharma</p>
+        <div><Link href ="https://github.com/">Github Link</Link></div></>
+    )
+}
