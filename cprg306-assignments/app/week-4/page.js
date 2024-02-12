8fb4275a78da41960938eb1be85b6f11403718b3
Gourav Sharma <gourav.sharma@edu.sait.ca>
2024-02-12 12:29:09 -0700

    pushed

---
+++ cprg306-assignments/app/week-4/page.js
@@ -0,0 +1,11 @@
+
+import NewItem from "./new-item";
+
+export default function Page() {
+    return (
+        <div>
+            <h1>New Item Form</h1>
+            <NewItem/>
+        </div>
+    );
+}
